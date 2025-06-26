8ae054ff3d55f2cd55e16a42ebd2d631b247ea09
Tharusha Dilshan <tharushadil11@gmail.com>
2025-06-26 14:25:28 +0530

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,10 +4,17 @@ import { useEffect, useState } from 'react';
 import ProfileLayout from './ProfileLayout';
 import { decodeToken } from '@/utils/decodeToken';
 
+type DecodedUser = {
+  given_name: string;
+  email?: string;
+  sub?: string;
+  // add more if needed
+};
+
 export default function ProfilePage() {
  // const { user } = useAuth();
 
-  const[user, setUser] = useState('')
+const [user, setUser] = useState<DecodedUser | null>(null);
 
   useEffect(() => {
   const token = localStorage.getItem('idToken');
@@ -23,7 +30,6 @@ export default function ProfilePage() {
   }
 }, []);
 
-console.log('user 12', user.given_name)
 
   return (
     <ProfileLayout user={user} />
